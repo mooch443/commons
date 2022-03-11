fa6c83b324893a8cebabc78fd6beefad4e8d2ca7
DESKTOP-J587J7M\tristan <locusts@trex.run>
2022-03-11 15:47:35 +0100

    test_python update

--- examples/test_python.cpp
+++ examples/test_python.cpp
@@ -1,19 +1,30 @@
 #include <pybind11/embed.h>
+#include <iostream>
 namespace py = pybind11;
 
 int main() {
-    
-
-    std::cout << Py_GetPythonHome() << std::endl;
+    auto str = [](const wchar_t* input) {
+        return input ? std::wstring(input) : std::wstring();
+    };
+    std::wcout << str(Py_GetPythonHome()) << std::endl;
     std::cout << Py_GetVersion() << std::endl;
-    std::cout << Py_GetPath() << std::endl;
-    std::cout << Py_GetPlatform() << std::endl;
-    std::cout << Py_GetExecPrefix() << std::endl;
+    std::wcout << str(Py_GetPath()) << std::endl;
+    std::wcout << Py_GetPlatform() << std::endl;
+    std::wcout << str(Py_GetExecPrefix()) << std::endl;
     
     py::scoped_interpreter guard{};
-    py::exec(R"(
-        kwargs = dict(name="World", number=42)
-        message = "Hello, {name}! The answer is {number}".format(**kwargs)
-        print(message)
-    )");
+    try {
+        py::exec(R"(
+            kwargs = dict(name="World", number=42)
+            message = "Hello, {name}! The answer is {number}".format(**kwargs)
+            print(message)
+            import numpy as np
+            import tensorflow as tf
+            print(tf)
+        )");
+    }
+    catch (py::error_already_set& e) {
+        std::cout << "Error already set: " << e.what() << std::endl;
+        e.restore();
+    }
 }
