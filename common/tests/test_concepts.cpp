807c8ca3c9bad381833482e5d92e78ea17ad131a
Tristan Walter <a@trex.run>
2022-03-15 12:09:15 +0100

    * fixing test program

--- common/tests/test_concepts.cpp
+++ common/tests/test_concepts.cpp
@@ -1,5 +1,5 @@
 #include <concepts>
 
 int main() {
-	static_assert(std::convertible_to<int, int>, "Convertible_to not available.");
+	static_assert(std::convertible_to<int, int>, "std::convertible_to not available.");
 }
