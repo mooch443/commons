da563a419a3ec7935d177bc77e74cab82229592b
Tristan Walter <twalter@ab.mpg.de>
2022-09-22 12:26:18 +0200

    * few tests

--- common/gui/DrawBase.cpp
+++ common/gui/DrawBase.cpp
@@ -6,25 +6,31 @@ namespace gui {
     std::function<Bounds(const std::string&, Drawable*, const Font&)> _restore_line_bounds;
     std::function<uint32_t(const Font&)> _restore_line_spacing;
 
-    std::function<uint32_t(const Font&)> line_spacing_fn = [](const Font& font) -> uint32_t
-    {
-        return narrow_cast<uint32_t>(roundf(25 * font.size));
-    };
+    std::function<uint32_t(const Font&)>& line_spacing_fn(){
+        static std::function<uint32_t(const Font&)> fn = [](const Font& font) -> uint32_t
+        {
+            return narrow_cast<uint32_t>(roundf(25 * font.size));
+        };
+        return fn;
+    }
     void Base::set_default_line_spacing(std::function<uint32_t(const Font&)> fn) {
-        line_spacing_fn = fn;
+        line_spacing_fn() = fn;
     }
     uint32_t Base::default_line_spacing(const Font &font) {
-        return line_spacing_fn(font);
+        return line_spacing_fn()(font);
     }
 
-    std::function<Bounds(const std::string&, Drawable*, const Font&)> text_bounds_fn = [](const std::string& text, Drawable*, const Font& font) -> Bounds {
-        return Bounds(0, 0, text.length() * 11.3f * font.size, line_spacing_fn(font));
-    };
+    auto& text_bounds_fn() {
+        static std::function<Bounds(const std::string&, Drawable*, const Font&)> fn = [](const std::string& text, Drawable*, const Font& font) -> Bounds {
+            return Bounds(0, 0, text.length() * 11.3f * font.size, line_spacing_fn()(font));
+        };
+        return fn;
+    }
     Bounds Base::default_text_bounds(const std::string &text, Drawable* obj, const Font& font) {
-        return text_bounds_fn(text, obj, font);
+        return text_bounds_fn()(text, obj, font);
     }
     void Base::set_default_text_bounds(std::function<Bounds (const std::string &, Drawable *, const Font &)> fn) {
-        text_bounds_fn = fn;
+        text_bounds_fn() = fn;
     }
 
     uint32_t Base::line_spacing(const Font& font) {
@@ -42,8 +48,8 @@ namespace gui {
 
     Base::Base() {
         _previous_base = _latest_base;
-        _previous_line_spacing = line_spacing_fn;
-        _previous_line_bounds = text_bounds_fn;
+        _previous_line_spacing = line_spacing_fn();
+        _previous_line_bounds = text_bounds_fn();
         
         _restore_line_bounds = _previous_line_bounds;
         _restore_line_spacing = _previous_line_spacing;
@@ -68,7 +74,7 @@ namespace gui {
                 };
             _restore_line_bounds = [](const std::string& text, Drawable*, const Font& font) -> Bounds
                 {
-                    return Bounds(0, 0, text.length() * 11.3f * font.size, line_spacing_fn(font));
+                    return Bounds(0, 0, text.length() * 11.3f * font.size, line_spacing_fn()(font));
                 };
             
             _latest_base = _previous_base;
