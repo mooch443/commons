2e83025bca9e54463a7388e99ce317d6ea43b5b9
Tristan Walter <twalter@ab.mpg.de>
2023-06-08 02:01:32 +0200

    * datalocation instance for windows (mainly)

--- common/file/DataLocation.h
+++ common/file/DataLocation.h
@@ -6,11 +6,17 @@
 namespace file {
 
 class DataLocation {
+    std::mutex location_mutex;
+    std::map<std::string, std::function<file::Path(file::Path)>> location_funcs;
 public:
     static void register_path(std::string purpose, std::function<file::Path(file::Path)> fn);
     static void replace_path(std::string purpose, std::function<file::Path(file::Path)> fn);
     static file::Path parse(const std::string& purpose, file::Path path = file::Path());
     static bool is_registered(std::string purpose);
+
+    //! managing instances in order to support Windows DLLs
+    static DataLocation* instance();
+    static void set_instance(DataLocation* instance);
 private:
     DataLocation() {}
 };
