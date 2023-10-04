ba97285a974b0eef177823c7d7f89bbad5b7bd1b
Tristan Walter <twalter@ab.mpg.de>
2023-10-04 15:28:04 +0200

    * update script

--- .github/scripts/update-readme.py
+++ .github/scripts/update-readme.py
@@ -1,19 +1,24 @@
 import re
 
-def update_code_block(file_path, marker):
+def update_code_block(file_path, marker, ticks='```'):
     with open(file_path, 'r') as f:
         new_content = f.read()
 
     with open('README.md', 'r') as f:
         readme_content = f.read()
 
-    pattern = re.compile(f"{marker}\n\n```.*?```\n", re.DOTALL)
-    replacement = f"{marker}\n\n```cpp\n{new_content}\n```\n"
+    # Updated pattern to look for marker, then ```
+    # and capture everything until another ```
+    pattern = re.compile(f"({marker}.*?{ticks}\\n)(.*?)(```)", re.DOTALL)
+    
+    # Updated replacement to keep the existing marker and ticks,
+    # but replace the content in between
+    replacement = f"\\1{new_content}\n\\3"
     readme_content = re.sub(pattern, replacement, readme_content)
 
     with open('README.md', 'w') as f:
         f.write(readme_content)
 
 if __name__ == "__main__":
-    update_code_block('examples/simple_gui.cpp', '## Example: Dynamic GUI')
-    update_code_block('examples/test_gui.json', '```json')
+    update_code_block('examples/simple_gui.cpp', 'Example: Dynamic GUI', '```cpp')
+    update_code_block('examples/test_gui.json', 'test_gui.json', '```json')
