cc9b54933ff911e340f2dc9b955c057f67439db7
Tristan Walter <twalter@ab.mpg.de>
2023-10-04 15:15:12 +0200

    * test

--- .github/scripts/update-readme.py
+++ .github/scripts/update-readme.py
@@ -7,18 +7,13 @@ def update_code_block(file_path, marker):
     with open('README.md', 'r') as f:
         readme_content = f.read()
 
-    # Updated pattern to look for marker, then ```
-    # and capture everything until another ```
-    pattern = re.compile(f"({marker}.*?```\\n)(.*?)(```)", re.DOTALL)
-    
-    # Updated replacement to keep the existing marker and ticks,
-    # but replace the content in between
-    replacement = f"\\1{new_content}\n\\3"
+    pattern = re.compile(f"{marker}\n\n```.*?```\n", re.DOTALL)
+    replacement = f"{marker}\n\n```cpp\n{new_content}\n```\n"
     readme_content = re.sub(pattern, replacement, readme_content)
 
     with open('README.md', 'w') as f:
         f.write(readme_content)
 
 if __name__ == "__main__":
-    update_code_block('examples/simple_gui.cpp', 'Example: Dynamic GUI')
-    update_code_block('examples/test_gui.json', 'test_gui.json')
+    update_code_block('examples/simple_gui.cpp', '## Example: Dynamic GUI')
+    update_code_block('examples/test_gui.json', '```json')
