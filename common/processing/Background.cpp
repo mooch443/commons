d650c061a8c60ea6f2b8ad2449a2e0876dbdea1a
Tristan Walter <twalter@ab.mpg.de>
2024-02-02 22:01:24 +0100

    * need to use something that works also for DLLs

--- common/processing/Background.cpp
+++ common/processing/Background.cpp
@@ -3,19 +3,48 @@
 
 namespace cmn {
     static std::atomic<bool> track_absolute_difference = true,
-                             track_background_subtraction = true;
+        track_background_subtraction = true;
     static std::atomic<meta_encoding_t::data::values> meta_encoding = cmn::meta_encoding_t::gray.value();
 
+    static auto check_callbacks = []() {
+        static std::once_flag _check_callbacks;
+        std::call_once(_check_callbacks, []() {
+            auto _callback = GlobalSettings::map().register_callbacks({
+                "track_absolute_difference",
+                "track_background_subtraction",
+                "meta_encoding"
+
+            }, [](std::string_view name) {
+                if (name == "track_absolute_difference") {
+                    cmn::track_absolute_difference = SETTING(track_absolute_difference).value<bool>();
+                }
+                else if (name == "track_background_subtraction") {
+                    cmn::track_background_subtraction = SETTING(track_background_subtraction).value<bool>();
+                }
+                else if (name == "meta_encoding") {
+                    cmn::meta_encoding = SETTING(meta_encoding).value<meta_encoding_t::Class>().value();
+
+                    print("updated meta_encoding to ", meta_encoding_t::Class(cmn::meta_encoding.load()));
+                }
+                //Background::update_callback();
+            });
+		});
+    };
+
     bool Background::track_absolute_difference() {
+        check_callbacks();
         return cmn::track_absolute_difference;
     }
     bool Background::track_background_subtraction() {
+        check_callbacks();
         return cmn::track_background_subtraction;
     }
     meta_encoding_t::data::values Background::meta_encoding() {
+        check_callbacks();
         return cmn::meta_encoding;
     }
     ImageMode Background::image_mode() {
+        check_callbacks();
         return cmn::meta_encoding == meta_encoding_t::gray
             ? ImageMode::GRAY
             : ImageMode::R3G3B2;
@@ -24,21 +53,7 @@ namespace cmn {
     Background::Background(Image::Ptr&& image, LuminanceGrid *grid)
         : _image(std::move(image)), _grid(grid), _bounds(_image->bounds())
     {
-        auto _callback = GlobalSettings::map().register_callbacks({
-            "track_absolute_difference", 
-            "track_background_subtraction",
-            "meta_encoding"
-            
-        }, [this](std::string_view name) {
-            if(name == "track_absolute_difference") {
-                cmn::track_absolute_difference = SETTING(track_absolute_difference).value<bool>();
-            } else if(name == "track_background_subtraction") {
-                cmn::track_background_subtraction = SETTING(track_background_subtraction).value<bool>();
-            } else if(name == "meta_encoding") {
-                cmn::meta_encoding = SETTING(meta_encoding).value<meta_encoding_t::Class>().value();
-            }
-            update_callback();
-        });
+        
     }
     
     Background::~Background() {
