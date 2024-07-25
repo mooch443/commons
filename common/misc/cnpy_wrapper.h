150b4eab3555e29a3efa151bf5497c2ee248eb56
Tristan Walter <twalter@ab.mpg.de>
2024-07-25 22:50:45 +0200

    * adding cnpy wrapper for strings

--- common/misc/cnpy_wrapper.h
+++ common/misc/cnpy_wrapper.h
@@ -8,17 +8,31 @@ namespace cmn {
         try {
             cnpy::npz_save(zipname, fname, data, shape, mode);
         } catch(const std::runtime_error& e) {
-            throw U_EXCEPTION("Exception while saving '",zipname,"': ",e.what(),"");
+            throw U_EXCEPTION("Exception while saving ",zipname,": ",e.what(),"");
         } catch(...) {
             throw U_EXCEPTION("Unknown exception while saving ",zipname,".");
         }
     }
     
-    template<typename T> void npz_save(std::string zipname, std::string fname, const std::vector<T> data, std::string mode = "w") {
+    template<typename T> void npz_save(std::string zipname, std::string fname, const std::vector<T>& data, std::string mode = "w") {
         try {
             cnpy::npz_save(zipname, fname, data, mode);
         } catch(const std::runtime_error& e) {
-            throw U_EXCEPTION("Exception while saving '",zipname,"': ",e.what(),"");
+            throw U_EXCEPTION("Exception while saving ",zipname,": ",e.what(),"");
+        } catch(...) {
+            throw U_EXCEPTION("Unknown exception while saving ",zipname,".");
+        }
+    }
+
+    inline void npz_save(std::string zipname, std::string fname, const std::string& data, std::string mode = "w") {
+        try {
+            // Convert std::string to a std::vector<char>
+            std::vector<char> charArray(data.begin(), data.end());
+
+            // Save the char array to an NPZ file using cnpy
+            cnpy::npz_save(zipname, fname, charArray.data(), {charArray.size()}, mode);
+        } catch(const std::runtime_error& e) {
+            throw U_EXCEPTION("Exception while saving ",zipname,": ",e.what(),"");
         } catch(...) {
             throw U_EXCEPTION("Unknown exception while saving ",zipname,".");
         }
