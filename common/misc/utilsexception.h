b2d48973a6ddea6addb973d45abba500b522716e
Tristan Walter <twalter@ab.mpg.de>
2024-08-05 04:18:54 +0200

    * pasting everything here

--- common/misc/utilsexception.h
+++ common/misc/utilsexception.h
@@ -4,18 +4,6 @@
 
 namespace cmn {
 
-class UtilsException : public std::exception {
-public:
-
-	UtilsException(const std::string& str);
-
-	~UtilsException() throw();
-
-	virtual const char * what() const throw();
-
-private:
-	std::string msg;
-};
 
 }
 
