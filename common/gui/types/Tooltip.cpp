0682e0ec88e46dd3a2286ac173e9d1bd7b583d9c
Tristan Walter <twalter@ab.mpg.de>
2024-11-06 17:51:02 +0100

    * fixing tooltips not being displayed in frame

--- common/gui/types/Tooltip.cpp
+++ common/gui/types/Tooltip.cpp
@@ -44,24 +44,54 @@ namespace cmn::gui {
             mp = tf.transformPoint(mp) + Vec2(5, 0);
         }
         
-        float ox = 0;
-        if(stage() && mp.x > stage()->dialog_window_size().width * 0.5) {
+        auto window = stage()->dialog_window_size();
+        auto ox = 0_F;
+        if(stage() && mp.x > window.width * 0.5) {
             ox = 1;
         }
 
-        if(mp.y < stage()->dialog_window_size().height * 0.5)
+        if(mp.y < window.height * 0.5)
             set_origin(Vec2(ox, 0));
         else
             set_origin(Vec2(ox, 1));
         
+        auto o = origin();
+        auto text_dims = _text.size();
+        if(stage())
+            text_dims = text_dims.div(stage()->scale());
+        text_dims += Vec2(10);
+        
+        if(o.y == 0
+           && mp.y + text_dims.height > window.height)
+        {
+            mp.y = window.height - text_dims.height;
+            
+        } else if(o.y == 1
+                  && mp.y - text_dims.height < 0)
+        {
+            mp.y = text_dims.height;
+        }
+        
+        if(o.x == 0
+           && mp.x + text_dims.width > window.width)
+        {
+            mp.x = window.width - text_dims.width;
+            
+        } else if(o.x == 1
+                  && mp.x - text_dims.width < 10)
+        {
+            mp.x = text_dims.width + 10;
+        }
+        
         if(!content_changed()) {
-            set_pos(mp);
+            set_pos(mp); /// only set position here and exist
             return;
         }
         
         auto ctx = OpenContext();
         advance_wrap(_text);
         
+        /// set both position and size
         set_bounds(Bounds(mp, _text.size() + Vec2(5, 2) * 2));
     }
     
